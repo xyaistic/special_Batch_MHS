fd4c2584dce289d7e63ea4800acb86b09f9e6fba
Adeeb <adeebkhan3463@gmail.com>
2024-11-18 16:08:45 +0530

    changes

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react'
+import {Link} from "react-router-dom"
 // import logo from "../Img/logo.png"
 import { CiHeart } from "react-icons/ci";
 import { GiShoppingCart } from "react-icons/gi";
